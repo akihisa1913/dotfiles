6ef306629a502582065a00319f13238c652011f2
akihisa1913 <>
2026-07-14 22:05:05 +0900

    nvimの行番号を見やすくする

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,17 +1,24 @@
 return {
-  "folke/tokyonight.nvim",
-  lazy = false,
-  priority = 1000,
-  opts = {
-    style = "moon",
-    transparent = true,
-    styles = {
-      sidebars = "dark",
-      floats = "dark",
-    },
-  },
-  config = function(_, opts)
-    require("tokyonight").setup(opts)
-    vim.cmd.colorscheme("tokyonight")
-  end,
+	"folke/tokyonight.nvim",
+	lazy = false,
+	priority = 1000,
+	opts = {
+		style = "moon",
+		transparent = true,
+		styles = {
+			sidebars = "dark",
+			floats = "dark",
+		},
+		on_highlights = function(hl, c)
+			-- 通常の行番号を明るく（透過背景でも読めるように）
+			hl.LineNr = { fg = c.fg_dark }
+			-- 相対行番号の上下も少し明るく
+			hl.LineNrAbove = { fg = c.fg_dark }
+			hl.LineNrBelow = { fg = c.fg_dark }
+		end,
+	},
+	config = function(_, opts)
+		require("tokyonight").setup(opts)
+		vim.cmd.colorscheme("tokyonight")
+	end,
 }
