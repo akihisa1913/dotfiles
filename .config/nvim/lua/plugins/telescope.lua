67e0494346cf57872917637f0755ca814c7b1d10
akihisa1913 <>
2026-07-16 02:29:50 +0900

    telescope検索で戻れるようにする

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -6,5 +6,6 @@ return {
     { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Live Grep" },
     { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
     { "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "Help Tags" },
+    { "<leader>fr", "<cmd>Telescope resume<cr>", desc = "Resume Last Search" },
   },
 }
